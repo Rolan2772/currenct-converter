955c5771b445653f28873ced2289875fec9c4432
Rolan Burykin <Rolan.Burykin@kstt.ua>
2018-01-17 19:01:20 +0200

    CI/CD setup

---
+++ src/test/java/com/zooplus/sdc/converter/controllers/HomeControllerTest.java
@@ -0,0 +1,33 @@
+package com.zooplus.sdc.converter.controllers;
+
+import com.zooplus.sdc.converter.controller.HomeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void nextRound() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello world !!!!"));
+    }
+}
