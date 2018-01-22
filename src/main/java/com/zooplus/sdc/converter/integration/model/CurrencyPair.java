57fdbab4a7062de8b0217ecdbbd0ecec7ea3046b
Rolan Burykin <Rolan.Burykin@kstt.ua>
2018-01-22 11:43:04 +0200

    Case insensitive currency name.

--- src/main/java/com/zooplus/sdc/converter/integration/model/CurrencyPair.java
+++ src/main/java/com/zooplus/sdc/converter/integration/model/CurrencyPair.java
@@ -2,11 +2,22 @@ package com.zooplus.sdc.converter.integration.model;
 
 import lombok.Value;
 
+import javax.validation.constraints.NotNull;
+
 @Value(staticConstructor = "of")
 public class CurrencyPair {
 
     private String baseCurrency;
     private String targetCurrency;
+
+    private CurrencyPair(@NotNull String baseCurrency, @NotNull String targetCurrency) {
+        this.baseCurrency = baseCurrency.toUpperCase();
+        this.targetCurrency = targetCurrency.toUpperCase();
+    }
+
+    public static CurrencyPair of(@NotNull String baseCurrency, @NotNull String targetCurrency) {
+        return new CurrencyPair(baseCurrency, targetCurrency);
+    }
 
     @Override
     public String toString() {
