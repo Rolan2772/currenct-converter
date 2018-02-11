fdffb59a270d5aed2f06ba1030c75934bcbc0fcd
Rolan Burykin <burykin.rolan@gmail.com>
2018-02-11 12:01:56 +0200

    Dev configuration for CORS

--- frontend/src/api/httpResources.js
+++ frontend/src/api/httpResources.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-import router from "../router/index"
 
-const BACK_END_URL = 'http://localhost:8081';
-const FRONT_END_URL = 'http://localhost:8080';
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = 'http://localhost:8081';
+  axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:8080';
+}
 
 // axios.interceptors.response.use((response) => response,
 //   (error) => {
@@ -43,17 +44,10 @@ const FRONT_END_URL = 'http://localhost:8080';
 //   }
 // )
 
-let BASE_REQUEST = axios.create({
-  baseURL: BACK_END_URL,
-  headers: {
-    'Access-Control-Allow-Origin': FRONT_END_URL
-  }
-});
-
 export const auth = {
 
   login: function (credentials) {
-    return BASE_REQUEST.post('login', 'userName=' + credentials.login, {
+    return axios.post('login', 'userName=' + credentials.login, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
@@ -61,11 +55,11 @@ export const auth = {
   },
 
   signUp: function (credentials) {
-    return BASE_REQUEST.post('users/signup', credentials)
+    return axios.post('users/signup', credentials)
   },
 
   getUser: function () {
-    return BASE_REQUEST.get('users/current')
+    return axios.get('users/current')
   }
 };
 
